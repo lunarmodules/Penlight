aba16beb14cb00fb58454f9b0b20f2129182fd69
steve donovan <steve.j.donovan@gmail.com>
2011-07-08 15:29:26 +0200

    more lexer patches - string tokens broken w/ some combinations

--- lua/pl/lexer.lua
+++ lua/pl/lexer.lua
@@ -44,9 +44,9 @@ local NUMBER4 = '^%d+%.?%d*[eE][%+%-]?%d+'
 local NUMBER5 = '^%d+%.?%d*'
 local IDEN = '^[%a_][%w_]*'
 local WSPACE = '^%s+'
-local STRING1 = "^'.-[^\\]'"
-local STRING2 = '^".-[^\\]"'
-local STRING3 = '^[\'"][\'"]'
+local STRING1 = [[^'.-[^\\]']]
+local STRING2 = [[^".-[^\\]"]]
+local STRING3 = "^((['\"])%2)" -- empty string
 local PREPRO = '^#.-[^\\]\n'
 
 local plain_matches,lua_matches,cpp_matches,lua_keyword,cpp_keyword
@@ -309,6 +309,7 @@ function lexer.lua(s,filter,options)
             {'^<=',tdump},
             {'^>=',tdump},
             {'^%.%.%.',tdump},
+            {'^%.%.',tdump},
             {'^.',tdump}
         }
     end
