lunarmodules/Penlight

commit 0b5b45414420bc0a7afcdfe027c8657d20fc39c0

utils.array_tostring converts values to strings; List and Set now use this

lua/pl/Set.lua
@@ -21,12 +21,12 @@
 
 local tablex = require 'pl.tablex'
 local utils = require 'pl.utils'
-local stdmt = utils.stdmt
+local array_tostring, concat = utils.array_tostring, table.concat
 local tmakeset,deepcompare,merge,keys,difference,tupdate = tablex.makeset,tablex.deepcompare,tablex.merge,tablex.keys,tablex.difference,tablex.update
 local Map = require 'pl.Map'
 local class = require 'pl.class'
+local stdmt = utils.stdmt
 local Set = stdmt.Set
-local list_loaded
 
 -- the Set class --------------------
 class(Map,nil,Set)
@@ -52,10 +52,7 @@ function Set:_init (t)
 end
 
 function Set:__tostring ()
-    if not list_loaded then
-        list_loaded = require 'pl.List'
-    end
-    return '['..Set.values(self):join ','..']'
+    return '['..concat(array_tostring(Set.values(self),','))..']'
 end
 
 --- get a list of the values in a set.

lua/pl/List.lua
@@ -26,10 +26,7 @@ local filter,imap,imap2,reduce,transform,tremovevalues = tablex.filter,tablex.im
 local tablex = tablex
 local tsub = tablex.sub
 local utils = require 'pl.utils'
-local function_arg = utils.function_arg
-local is_type = utils.is_type
-local split = utils.split
-local assert_arg = utils.assert_arg
+local array_tostring,split,is_type,assert_arg,function_arg = utils.array_tostring,utils.split,utils.is_type,utils.assert_arg,utils.function_arg
 local normalize_slice = tablex._normalize_slice
 
 --[[
@@ -382,7 +379,7 @@ end
 function List:join (delim)
     delim = delim or ''
     assert_arg(1,delim,'string')
-    return concat(imap(tostring,self),delim)
+    return concat(array_tostring(self),delim)
 end
 
 --- join a list of strings. <br>

lua/pl/utils.lua
@@ -205,6 +205,20 @@ function utils.splitv (s,re)
     return unpack(utils.split(s,re))
 end
 
+--- convert an array of values to strings.
+-- @param t a list-like table
+-- @param temp buffer to use, otherwise allocate
+-- @param tostr custom tostring function, called with (value,index).
+-- Otherwise use `tostring`
+-- @return the converted buffer
+function utils.array_tostring (t,temp,tostr)
+    temp, tostr = temp or {}, tostr or tostring
+    for i = 1,#t do
+        temp[i] = tostr(t[i],i)
+    end
+    return temp
+end
+
 local lua51_load = load
 
 if utils.lua51 then -- define Lua 5.2 style load()

tests/test-data.lua
@@ -9,6 +9,18 @@ open = stringio. open
 asserteq = require 'pl.test' . asserteq
 T = require 'pl.test'. tuple
 
+--[=[
+dat,err = data.read(open [[
+1.0 0.1
+0.2 1.3
+]])
+
+if err then print(err) end
+
+require 'pl.pretty'.dump(dat)
+os.exit(0)
+--]=]
+
 -- tab-separated data, explicit column names
 t1f = open [[
 EventID	Magnitude	LocationX	LocationY	LocationZ	LocationError	EventDate	DataFile
@@ -162,10 +174,12 @@ asserteq(dat:column_by_name(2), L{2,4,6,8,10,12,14,16,18,20})
 local q = dat:select '$3,2*$4 where $1 == 8'
 asserteq(T(q()),T(24,64))
 
-dat = data.read(open [[
+dat,err = data.read(open [[
 1.0 0.1
 0.2 1.3
 ]])
+
+if err then print(err) end
 
 -- if a method cannot be found, then we look up in array2d
 -- array2d.flatten(t) makes a 1D list out of a 2D array,

tests/test-pylib.lua
@@ -12,18 +12,19 @@ s = List{1,2,3,4,5}
 local lst = List:new()
 lst:append(10)
 lst:extend{20,30,40,50}
-assert (lst == List{10,20,30,40,50})
+asserteq (lst,List{10,20,30,40,50})
 lst:insert(3,11)
 lst:remove_value(40)
-assert (lst == List{10,20,11,30,50})
+asserteq (lst,List{10,20,11,30,50})
 local q=lst:pop()
-assert( lst:index(30)==4 )
-assert( lst:count(10)==1 )
+asserteq( lst:index(30),4 )
+asserteq( lst:count(10),1 )
 lst:sort()
 lst:reverse()
-assert (lst == List{30,20,11,10})
-assert (lst[#lst] == 10)
-assert (lst[#lst-2] == 20)
+asserteq (lst , List{30,20,11,10})
+asserteq (lst[#lst] , 10)
+asserteq (lst[#lst-2] , 20)
+asserteq (tostring(lst) , '{30,20,11,10}')
 
 lst = List {10,20,30,40,50}
 asserteq (lst:slice(2),{20,30,40,50})
@@ -38,11 +39,11 @@ asserteq(List.range(0,8,2),{0,2,4,6,8})
 asserteq(List.range(0,1,0.2),{0,0.2,0.4,0.6,0.8,1},1e-9)
 
 
-assert(lst == seq)
+asserteq(lst, seq)
 asserteq (List('abcd'),List{'a','b','c','d'})
 ls = List{10,20,30,40}
 ls:slice_assign(2,3,{21,31})
-assert (ls == List{10,21,31,40})
+asserteq (ls , List{10,21,31,40})
 -- strings ---
 s = '123'
 assert (s:isdigit())
@@ -52,13 +53,13 @@ assert (s:startswith('here'))
 assert (s:endswith('dog'))
 assert (s:count('dog') == 2)
 s = '  here we go    '
-assert (s:lstrip() == 'here we go    ')
-assert (s:rstrip() == '  here we go')
-assert (s:strip() == 'here we go')
-assert (('hello'):center(20,'+') == '++++++++hello+++++++')
+asserteq (s:lstrip() , 'here we go    ')
+asserteq (s:rstrip() , '  here we go')
+asserteq (s:strip() , 'here we go')
+asserteq (('hello'):center(20,'+') , '++++++++hello+++++++')
 
 t = Template('${here} is the $answer')
-assert(t:substitute {here = 'one', answer = 'two'} == 'one is the two')
+asserteq(t:substitute {here = 'one', answer = 'two'} , 'one is the two')
 
-assert (('hello dolly'):title() == 'Hello Dolly')
-assert (('h bk bonzo TOK fred m'):title() == 'H Bk Bonzo Tok Fred M')
+asserteq (('hello dolly'):title() , 'Hello Dolly')
+asserteq (('h bk bonzo TOK fred m'):title() , 'H Bk Bonzo Tok Fred M')

tests/test-set.lua
@@ -18,6 +18,10 @@ asserteq(S{1,2} + S{2,3},S{1,2,3})
 asserteq(S{1,2} * S{2,3}, S{2})
 -- symmetric_difference
 asserteq(S{1,2} ^ S{2,3}, S{1,3})
+-- tostring - illustrative, because these assertions may or may not work,
+-- due to no ordering in set elements
+--asserteq(tostring(S{1,2}),'[1,2]')
+--asserteq(tostring(S{1,S{2,3}}),'[1,[2,3]]')
 
 m = M{one=1,two=2}
 asserteq(m,M{one=1,two=2})

commit 4af18fc58a9cf4d95f629ad9b404c0174286c3fa

Set.__tostring borked; Corrected typo introduced by commit 0b5b454. (thanks to ntd)

lua/pl/Set.lua
@@ -52,7 +52,7 @@ function Set:_init (t)
 end
 
 function Set:__tostring ()
-    return '['..concat(array_tostring(Set.values(self),','))..']'
+    return '['..concat(array_tostring(Set.values(self)),',')..']'
 end
 
 --- get a list of the values in a set.