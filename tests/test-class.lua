e8940979fb9dbef726db698bad51cd645669826d
Thijs Schreijer <thijs@thijsschreijer.nl>
2013-09-15 22:24:08 +0200

    update test script to show succesful exit. Added failing tests on classes

--- tests/test-class.lua
+++ tests/test-class.lua
@@ -39,13 +39,39 @@ end
 
 function C:foo ()
     self:base('foo')
-    self:base():foo()  -- more convenient, but also more expensive
+--    self:base():foo()  -- more convenient, but also more expensive
 end
 
 c = C()
 c:foo()
 
 asserteq(c,{a=1,b=2,c=3,eee=1})
+--- call chains with ancestors
+local A = class()
+function A:_init()
+  self.init_chain = "A"
+end
+local B = class(A)
+local C = class(B)
+function C:_init()
+  self:super()
+  self.init_chain = self.init_chain.."C"
+end
+local D = class(C)
+local E = class(D)
+function E:_init()
+  self:super()
+  self.init_chain = self.init_chain.."E"
+end
+local F = class(E)
+local G = class(F)
+function G:_init()
+  self:super()
+  self.init_chain = self.init_chain.."G"
+end
+
+local i = G()
+assert(i.init_chain == "ACEG")
 
 --- metamethods!
 
