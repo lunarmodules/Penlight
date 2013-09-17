9cf9946cf42d8a68c35b5ad2aa08d35035a6bf5b
Thijs Schreijer <thijs@thijsschreijer.nl>
2013-09-17 14:23:18 +0200

    removed double-up tests after merge

--- tests/test-class.lua
+++ tests/test-class.lua
@@ -46,32 +46,6 @@ c = C()
 c:foo()
 
 asserteq(c,{a=1,b=2,c=3,eee=1})
---- call chains with ancestors
-local A = class()
-function A:_init()
-  self.init_chain = "A"
-end
-local B = class(A)
-local C = class(B)
-function C:_init()
-  self:super()
-  self.init_chain = self.init_chain.."C"
-end
-local D = class(C)
-local E = class(D)
-function E:_init()
-  self:super()
-  self.init_chain = self.init_chain.."E"
-end
-local F = class(E)
-local G = class(F)
-function G:_init()
-  self:super()
-  self.init_chain = self.init_chain.."G"
-end
-
-local i = G()
-assert(i.init_chain == "ACEG")
 
 --- metamethods!
 
