c98820da848d5951af1c3ba250eca5a2b47ea856
mpeterv <mpeterval@gmail.com>
2015-09-10 13:27:56 +0300

    Move helper functions into main chunk in pl.url

--- lua/pl/url.lua
+++ lua/pl/url.lua
@@ -4,28 +4,32 @@
 
 local M = {}
 
+local function quote_char(c)
+    return string.format("%%%02X", string.byte(c))
+end
+
 --- Quote the url.
 -- @string s the string
 -- @bool quote_plus Use quote_plus rules
 function M.quote(s, quote_plus)
-    local function url_quote_char(c)
-        return string.format("%%%02X", string.byte(c))
-    end
-
     if not s or not type(s) == "string" then
     	return s
     end
 
     s = s:gsub("\n", "\r\n")
-    s = s:gsub("([^A-Za-z0-9 %-_%./])", url_quote_char)
+    s = s:gsub("([^A-Za-z0-9 %-_%./])", quote_char)
     if quote_plus then
         s = s:gsub(" ", "+")
-        s = s:gsub("/", url_quote_char)
+        s = s:gsub("/", quote_char)
     else
         s = s:gsub(" ", "%%20")
     end
 
     return s
+end
+
+local function unquote_char(h)
+    return string.char(tonumber(h, 16))
 end
 
 --- Unquote the url.
@@ -36,7 +40,7 @@ function M.unquote(s)
     end
 
     s = s:gsub("+", " ")
-    s = s:gsub("%%(%x%x)", function(h) return string.char(tonumber(h, 16)) end)
+    s = s:gsub("%%(%x%x)", unquote_char)
     s = s:gsub("\r\n", "\n")
 
     return s
