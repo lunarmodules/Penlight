4f4c40eb8218ebd028f7ab2780ebe0206b03e3fa
Dennis Schridde <dennis.schridde@uni-heidelberg.de>
2016-08-09 19:13:14 +0200

    Test compat.load with Lua 5.1 style function arguments

---
+++ tests/test-compat.lua
@@ -0,0 +1,17 @@
+local test = require 'pl.test'
+local asserteq = test.asserteq
+
+local compat = require "pl.compat"
+local coroutine = require "coroutine"
+
+local code_generator = coroutine.wrap(function()
+	local result = {"ret", "urn \"Hello World!\""}
+	for _,v in ipairs(result) do
+		coroutine.yield(v)
+	end
+	coroutine.yield(nil)
+end)
+
+local f, err = compat.load(code_generator)
+asserteq(err, nil)
+asserteq(f(), "Hello World!")
