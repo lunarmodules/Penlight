d90956418cdf9e315719a7e4ce314db6b512ae00
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2017-04-11 14:45:24 +0300

    change compat.execute to behave similarly on Lua 5.1 and Lua > 5.1.

--- tests/test-executeex.lua
+++ tests/test-executeex.lua
@@ -1,30 +1,29 @@
-local compat = require 'pl.compat'
 local path = require 'pl.path'
 local utils = require 'pl.utils'
 local asserteq = require 'pl.test'.asserteq
 
 local echo_lineending = "\n"
-local retcode_multiplier = 1
 if path.is_windows then
     echo_lineending = " \n"
-else
-    if compat.lua51 and not compat.jit52 then
-        retcode_multiplier = 256
-    end
 end
 
 local function test_executeex(cmd, expected_successful, expected_retcode, expected_stdout, expected_stderr)
     local successful, retcode, stdout, stderr = utils.executeex(cmd)
     asserteq(successful, expected_successful)
-    asserteq(retcode,    expected_retcode * retcode_multiplier)
+    asserteq(retcode,    expected_retcode)
     asserteq(stdout,     expected_stdout)
     asserteq(stderr,     expected_stderr)
 end
 
 -- Check the return codes
-test_executeex("exit",    true,   0, "", "")
-test_executeex("exit 0",  true,   0, "", "")
-test_executeex("exit 13", false, 13, "", "")
+test_executeex("exit",       true,    0, "", "")
+test_executeex("exit 0",     true,    0, "", "")
+test_executeex("exit 1",     false,   1, "", "")
+test_executeex("exit 13",    false,  13, "", "")
+test_executeex("exit 255",   false, 255, "", "")
+test_executeex("exit 256",   true,    0, "", "")
+test_executeex("exit 257",   false,   1, "", "")
+test_executeex("exit 3809",  false, 225, "", "")
 
 -- Check output strings
 test_executeex("echo stdout",                         true, 0, "stdout" .. echo_lineending, "")
