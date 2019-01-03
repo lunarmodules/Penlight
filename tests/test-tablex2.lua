ab44446d5dff2eb9defd69cb39b57778d049de48
Thijs Schreijer <thijs@thijsschreijer.nl>
2019-01-03 18:01:16 +0100

    rename test files to match the modules tested

--- tests/test-tablex2.lua
+++ tests/test-tablex2.lua
@@ -8,6 +8,6 @@ local t = {10,20,5,5,10,'one',555}
 local permutations = P.table(t)
 print('permutations',#permutations)
 for _,p in ipairs(permutations) do
-	if not T.compare_no_order(t,p) then return print 'different!' end
+	assert(T.compare_no_order(t,p), 'different!')
 end
 print 'DONE'
