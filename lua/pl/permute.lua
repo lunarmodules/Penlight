363dc138797490dae76078864739f696fecd2e33
Hisham Muhammad <hisham@gobolinux.org>
2020-09-23 21:03:36 -0300

    pl.permute: replace coroutine with iterator

--- lua/pl/permute.lua
+++ lua/pl/permute.lua
@@ -7,47 +7,63 @@ local utils = require 'pl.utils'
 local copy = tablex.deepcopy
 local append = table.insert
 local assert_arg = utils.assert_arg
-local co_create = coroutine.create
-local co_yield = coroutine.yield
-local co_resume = coroutine.resume
 
 
 local permute = {}
 
--- PiL, 9.3
-
-local permgen
-permgen = function (a, n, fn)
-  if n == 0 then
-    fn(a)
-  else
-    for i=1,n do
-      -- put i-th element as the last one
-      a[n], a[i] = a[i], a[n]
-
-      -- generate all permutations of the other elements
-      permgen(a, n - 1, fn)
-
-      -- restore i-th element
-      a[n], a[i] = a[i], a[n]
-
-    end
-  end
-end
 
 --- an iterator over all permutations of the elements of a list.
 -- Please note that the same list is returned each time, so do not keep references!
 -- @param a list-like table
 -- @return an iterator which provides the next permutation as a list
-function permute.iter (a)
+function permute.iter(a)
     assert_arg(1,a,'table')
-    local n = #a
-    local co = co_create(function () permgen(a, n, co_yield) end)
-    return function ()   -- iterator
-        local _, res = co_resume(co)
-        return res
+
+    local t = #a
+    local stack = { 1 }
+    local function iter()
+        local h = #stack
+        local n = t - h + 1
+
+        local i = stack[h]
+        if i > t then
+            return
+        end
+
+        if n == 0 then
+            table.remove(stack)
+            h = h - 1
+
+            stack[h] = stack[h] + 1
+            return a
+
+        elseif i <= n then
+
+            -- put i-th element as the last one
+            a[n], a[i] = a[i], a[n]
+
+            -- generate all permutations of the other elements
+            table.insert(stack, 1)
+
+        else
+
+            table.remove(stack)
+            h = h - 1
+
+            n = n + 1
+            i = stack[h]
+
+            -- restore i-th element
+            a[n], a[i] = a[i], a[n]
+
+            stack[h] = stack[h] + 1
+        end
+        return iter() -- tail-call
     end
+
+    return iter
 end
+
 
 --- construct a table containing all the permutations of a list.
 -- @param a list-like table
@@ -56,8 +72,9 @@ end
 function permute.table (a)
     assert_arg(1,a,'table')
     local res = {}
-    local n = #a
-    permgen(a,n,function(t) append(res,copy(t)) end)
+    for t in permute.iter(a) do
+        append(res,copy(t))
+    end
     return res
 end
 
